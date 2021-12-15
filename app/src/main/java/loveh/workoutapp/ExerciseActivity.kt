d1b8f632c4fbb9b20b73ec618bf6ded9a1d90a79
Love Hulterström <love.hulterstrom@guardtools.com>
2021-12-15 16:03:16 +0100

    init

---
+++ app/src/main/java/loveh/workoutapp/ExerciseActivity.kt
@@ -0,0 +1,22 @@
+package loveh.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import loveh.workoutapp.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding : ActivityExerciseBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
